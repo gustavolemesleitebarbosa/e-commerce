9eb9fca4082b98e570fe4cb7a1e9a33717278d07
GustavoLemesLB <gustavo.lemes@work.co>
2023-02-10 16:04:14 -0300

    plus one commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import  React from 'react'
 import '../styles/globals.css'
+import {Toaster} from 'react-hot-toast'
+import { Layout } from '../components'
+import { StateContext } from '../context/stateContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster/>
+         <Component {...pageProps} />
+       </Layout>
+     </StateContext>
+  )
 }
 
 export default MyApp
